db577e4b465173845c45b1c27048a191f829e1b4
Wim Haanstra <wim@haanstra.io>
2024-12-26 16:40:11 +0100

    Remove excessive logging

--- modbus-solarman.ts
+++ modbus-solarman.ts
@@ -30,9 +30,7 @@ class ModbusSolarman implements Device {
     const { unavailable_timeout } = this.provider.getConfig();
     if (this.lastSuccessfullRead) {
       const diff = DateTime.now().diff(this.lastSuccessfullRead, 'seconds').seconds;
-      const result = (diff < (unavailable_timeout ?? DEFAULT_UNAVAILABLE_TIMEOUT));
-      this.provider.logger.trace('isAvailable', this.lastSuccessfullRead, diff, unavailable_timeout, result);
-      return result;
+      return (diff < (unavailable_timeout ?? DEFAULT_UNAVAILABLE_TIMEOUT));
     }
     return false;
   }
@@ -231,15 +229,11 @@ class ModbusSolarman implements Device {
       return;
     }
 
-    this.provider.logger.trace('Reading registers');
-
     const { updateInterval } = this.provider.getConfig();
 
-    this.provider.logger.trace('readRegisters: waiting for runningRequest to turn false');
     while (this.runningRequest) {
       await delay(1000);
     }
-    this.provider.logger.trace('readRegisters: runningRequest is false');
 
     this.runningRequest = true;
 
